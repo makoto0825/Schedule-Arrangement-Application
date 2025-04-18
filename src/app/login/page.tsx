7398513b6a12de019d6deb39a434906280c76d99
myktsk <22167483+myktsk@users.noreply.github.com>
2025-04-18 15:34:15 -0400

    :art: define global button styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -69,7 +69,7 @@ const page = () => {
                 <button
                   formAction={login}
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                  className="button button-primary w-full"
                 >
                   Sign in
                 </button>
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -60,7 +60,7 @@ const Page = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-primary px-3 py-2 text-sm/6 font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+                  className="button button-primary w-full"
                   formAction={signup}
                 >
                   Sign Up
