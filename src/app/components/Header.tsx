4f2826b9ecf70feead6348cc8780648532df6aa5
myktsk <22167483+myktsk@users.noreply.github.com>
2025-04-21 20:26:19 -0400

    :sparkles: enable signing out

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,16 @@
+'use client';
 import Image from 'next/image';
 import Link from 'next/link';
 import { HiPlusSm } from 'react-icons/hi';
+import { createClient } from '../utils/supabase/client';
 
 export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
+  const signOut = async () => {
+    const supabase = await createClient();
+    await supabase.auth.signOut();
+    window.location.href = '/login';
+  };
+
   return (
     <div className="mx-auto px-4 sm:px-6 lg:px-8 pb-6 sm:pb-12">
       <div className="flex h-16 justify-between">
@@ -19,19 +27,24 @@ export const Header = ({ isLoggedIn }: { isLoggedIn: boolean }) => {
             </Link>
           </div>
         </div>
-        <div className="flex items-center">
-          <div className="shrink-0">
-            {isLoggedIn && (
-              <Link
-                href="/event/create"
-                className="button-with-icon button-primary"
-              >
-                <HiPlusSm aria-hidden="true" className="size-5" />
-                New Event
-              </Link>
-            )}
+        {isLoggedIn && (
+          <div className="flex items-center">
+            <Link
+              href="/event/create"
+              className="button-with-icon button-primary"
+            >
+              <HiPlusSm aria-hidden="true" className="size-5" />
+              New Event
+            </Link>
+            <button
+              type="button"
+              className="block rounded-lg px-3 py-2 text-gray-900 hover:bg-gray-200 ml-2"
+              onClick={signOut}
+            >
+              Sign Out
+            </button>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
